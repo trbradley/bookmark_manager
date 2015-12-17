eec229310b4511060b64387ef824dcb8e3e96c09
Thomas Bradley <bradley.tom.r@gmail.com>
2015-12-17 10:39:02 +0000

    Adds ability to view each link's tags alongside on homepage

--- app/app.rb
+++ app/app.rb
@@ -8,7 +8,6 @@ require './app/models/tag'
 class Manager < Sinatra::Base
   get '/links' do
     @links = Link.all
-    @tags = Tag.all
     erb :'links/index'
   end
 
@@ -17,7 +16,7 @@ class Manager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.create(url: params[:url], title: params[:title])
+    link = Link.new(url: params[:url], title: params[:title])
     params[:tags].split.each do |tag|
       link.tags << Tag.create(name: tag)
     end
